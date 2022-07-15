8ddae230ee389e709032cc872276c0a687ae35f4
Andres Guerra Montoya <aamontoya@eafit.edu.co>
2022-07-15 08:19:45 -0500

    Added portal in motion to Not Found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '@styles/NotFound.scss'
+import portal from '../assets/portal.png';
 
 const NotFound = () => {
   return (
     <section>
+      <img src={portal} alt= 'portal'/>
       <h1>404<br/>Not F<span className='not-found-morty'></span><span className='not-found-summer'></span>nd </h1>
     </section>
   )
